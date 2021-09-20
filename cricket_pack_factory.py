7a7c9f4b32b1885de20fbe66ab22aae1414d4b7a
Kevin Jackson <kjacksona7@gmail.com>
2021-09-20 23:16:58 +0100

    Corrected path to cricket csv file

--- cricket_pack_factory.py
+++ cricket_pack_factory.py
@@ -6,7 +6,7 @@ class CricketPackFactory(object):
     def __load_from_csv(self, file): 
 
         #Extract cards data into a panda dataframe and limit pack size
-        cards_df = pd.read_csv('Data/EnglandTestCricketerData.csv').head(32)
+        cards_df = pd.read_csv('EnglandTestCricketerData.csv').head(32)
 
         #Rank cards for each stat -- look to do this programmatically using order
         #cards_df['Runs scored Rank'] = cards_df['Runs scored'].rank(ascending=False)
